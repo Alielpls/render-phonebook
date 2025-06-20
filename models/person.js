17c9ad96528f69de42bd0fd3e689a7c6cb52da00
David Aliel <aliel.david.aliel@gmail.com>
2025-06-20 08:13:14 -0300

    Update person.js

--- models/person.js
+++ models/person.js
@@ -5,35 +5,41 @@ mongoose.set('strictQuery', false)
 //const password = process.argv[2]
 const url = process.env.MONGODB_URI
 mongoose.connect(url).then(
-    result =>{
-        console.log('connected!')
-    }
+  () => {
+    console.log('connected!')
+  }
 ).catch(
-    error =>{
-        console.log('error connecting', error.message)
-    }
+  error => {
+    console.log('error connecting', error.message)
+  }
 )
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        required:true,
-        minLength: 5
-    },
-    number:{
-        type:String,
-        required:true,
-        minLength:5
-    } 
+  name: {
+    type:String,
+    required:true,
+    minLength: 3
+  },
+  number:{
+    type:String,
+    required:true,
+    minLength:8,
+    validate:{
+      validator: (v) => {
+        return /^\d{2,3}-\d{4,}/.test(v)
+      },
+      message: p => `${p.value} is not a phone number!`
+    }
+  }
 })
 
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) =>{
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
